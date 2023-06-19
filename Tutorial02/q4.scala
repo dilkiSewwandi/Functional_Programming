49da472ede01cacb611532551f233bdfb6a2aa4d
Dilki Sewwandi <136557184+DilkiSewwwandi@users.noreply.github.com>
2023-06-19 21:54:14 +0530

    Update q4.scala

--- Tutorial02/q4.scala
+++ Tutorial02/q4.scala
@@ -1,16 +1,23 @@
 package Tutorial02
 
 object q4 {
+  var max = -1 
   def main(args: Array[String]): Unit = {
-    val normalPrice = 15.0
-    val averageAttendance = 120
-    val attendanceChange = 20.0
-    val performanceCost = 500
-    val attendeeCost = 3.0
-
-    println("Cost taken by the normal ticket price " + (normalCost(120)-attendanceCost()))
-
-
+    println(profit(5), profit(10), profit(15), profit(20))
+    println(profit(25), profit(30), profit(35), profit(40))
+    println(max)
   }
 
+  def attendees(price: Int): Int = 120 + (15 - price) / 5 * 20
+
+  def revenue(price: Int): Int = attendees(price) * price
+
+  def cost(price: Int): Int = 500 + attendees(price)
+
+  def profit(price: Int): Int = {
+    val currentProfit = revenue(price) - cost(price)
+    if (currentProfit > max)
+      max = currentProfit
+    currentProfit
+  }
 }
