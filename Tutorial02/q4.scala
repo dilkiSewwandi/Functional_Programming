77dede2232befe7e1468392a216090f3bcf51dcc
Dilki Sewwandi <136557184+DilkiSewwwandi@users.noreply.github.com>
2023-06-19 19:25:57 +0530

    Update q4.scala

--- Tutorial02/q4.scala
+++ Tutorial02/q4.scala
@@ -9,7 +9,7 @@ object q4 {
     val attendeeCost = 3.0
 
     println("Cost taken by the normal ticket price " + (normalCost(120)-attendanceCost()))
-println("Cost taken by the normal ticket price " + (normalCost(120)-attendanceCost()))
+
 
   }
 
